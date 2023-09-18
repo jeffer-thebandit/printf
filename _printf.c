e06c1767aa748e37f055dead78bd6537656f1f72
jeffer-thebandit <jeff39724@gmail.com>
2023-09-18 05:39:21 -0700

    this is commit for printf project

--- _printf.c
+++ _printf.c
@@ -1,66 +1,74 @@
-#include "main.h"
-
-void print_buffer(char buffer[], int *buff_ind);
+#include" main.h"
 
 /**
  * _printf - Printf function
  * @format: format.
  * Return: Printed chars.
  */
-int _printf(const char *format, ...)
-{
-	int i, printed = 0, printed_chars = 0;
-	int flags, width, precision, size, buff_ind = 0;
-	va_list list;
-	char buffer[BUFF_SIZE];
 
-	if (format == NULL)
-		return (-1);
-
-	va_start(list, format);
-
-	for (i = 0; format && format[i] != '\0'; i++)
-	{
-		if (format[i] != '%')
-		{
-			buffer[buff_ind++] = format[i];
-			if (buff_ind == BUFF_SIZE)
-				print_buffer(buffer, &buff_ind);
-			/* write(1, &format[i], 1);*/
-			printed_chars++;
-		}
-		else
-		{
-			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
-			size = get_size(format, &i);
-			++i;
-			printed = handle_print(format, &i, list, buffer,
-				flags, width, precision, size);
-			if (printed == -1)
-				return (-1);
-			printed_chars += printed;
-		}
-	}
-
-	print_buffer(buffer, &buff_ind);
-
-	va_end(list);
-
-	return (printed_chars);
+int print_char(va_list args) {
+    char ch = (char)va_arg(args, int);
+    write(STDOUT_FILENO, &ch, 1);
+    return 1;
 }
 
-/**
- * print_buffer - Prints the contents of the buffer if it exist
- * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
- */
-void print_buffer(char buffer[], int *buff_ind)
-{
-	if (*buff_ind > 0)
-		write(1, &buffer[0], *buff_ind);
-
-	*buff_ind = 0;
+int print_string(va_list args) {
+    const char *str = va_arg(args, const char *);
+    int count = 0;
+    while (*str) {
+        write(STDOUT_FILENO, str, 1);
+        str++;
+        count++;
+    }
+    return count;
 }
+
+int print_percent(va_list args) {
+    write(STDOUT_FILENO, "%", 1);
+    return 1;
+}
+
+int _printf(const char *format, ...) {
+    va_list args;
+    va_start(args, format);
+    int count = 0;
+
+    while (*format) {
+        if (*format == '%') {
+            format++; // Move past '%'
+            if (*format == 'c') {
+                count += print_char(args);
+            } else if (*format == 's') {
+                count += print_string(args);
+            } else if (*format == '%') {
+                count += print_percent(args);
+            } else {
+                // Invalid format specifier, print it as is
+                write(STDOUT_FILENO, "%", 1);
+                write(STDOUT_FILENO, format, 1);
+                count += 2;
+            }
+        } else {
+            // Print non-format characters
+            write(STDOUT_FILENO, format, 1);
+            count++;
+        }
+
+        format++;
+    }
+
+    va_end(args);
+    return count;
+}
+
+int main() {
+    int num = 42;
+    char ch = 'A';
+    const char *str = "Hello, World!";
+    
+    int count = _printf("This is a number: %d, a character: %c, and a string: %s%%\n", num, ch, str);
+    printf("\nTotal characters printed: %d\n", count);
+
+    return 0;
+}
+
